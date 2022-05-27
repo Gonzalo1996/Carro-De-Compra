6811ec9a37de4c290dece1aaa8d9b89e63ded6d0
Gonzalo1996 <gonald96@gmail.com>
2022-05-27 01:28:35 -0300

    Comienzo del proyecto.
    Agregado el index, css y js. Falta terminar el nav.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const contCarro = document.querySelector('.header_nav--carro');
+const carro = document.querySelector('.header_nav--carro i');
+/*
+contCarro.addEventListener('mouseover',cambioColorCarro);
+contCarro.addEventListener('mouseout',resetColorCarro);
+*/
+contCarro.addEventListener('click',prueba);
+
+/*
+function cambioColorCarro(){
+    this.style.backgroundColor = 'aqua';
+    this.style.transition ='.3s';
+    carro.style.color = 'grey';
+    carro.style.transition ='.3s';
+}
+
+function resetColorCarro(){
+    this.style.backgroundColor = 'transparent';
+    this.style.transition ='.3s';
+    carro.style.color = 'black';
+    carro.style.transition ='.3s';
+}
+*/
+function prueba(){
+    this.style.backgroundColor = 'aqua';
+    this.style.transition ='.3s';
+    carro.style.color = 'grey';
+    carro.style.transition ='.3s';
+}
