aaa45fe514ca34878b1be84b8b08c8aaff6c634a
Gonzalo1996 <gonald96@gmail.com>
2022-06-13 23:19:06 -0300

    Aplicando Bootstrap para galería y registro de compra.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const contCarro = document.querySelector('.header_nav--carro');
 const carro = document.querySelector('.header_nav--carro i');
-/*
+
 contCarro.addEventListener('mouseover',cambioColorCarro);
 contCarro.addEventListener('mouseout',resetColorCarro);
-*/
-contCarro.addEventListener('click',prueba);
+
 
 /*
+contCarro.addEventListener('click',prueba);
+*/
+
 function cambioColorCarro(){
     this.style.backgroundColor = 'aqua';
     this.style.transition ='.3s';
@@ -20,10 +22,12 @@ function resetColorCarro(){
     carro.style.color = 'black';
     carro.style.transition ='.3s';
 }
-*/
+
+/*
 function prueba(){
     this.style.backgroundColor = 'aqua';
     this.style.transition ='.3s';
     carro.style.color = 'grey';
     carro.style.transition ='.3s';
 }
+*/
